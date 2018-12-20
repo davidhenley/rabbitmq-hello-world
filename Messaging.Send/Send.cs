495d587cd3a91bb6a202969ec93a621770f0a5a2
David Henley <henleydr@gmail.com>
2018-12-19 18:42:38 -0600

    Init Commit

---
+++ Messaging.Send/Send.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Messaging.Send
+{
+    class Send
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost"
+            };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("hello", false, false, false, null);
+                    string message = "Hello World!";
+
+                    var body = Encoding.UTF8.GetBytes(message);
+                    channel.BasicPublish("", "hello", null, body);
+
+                    System.Console.WriteLine($" [x] Sent {message}");
+                }
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# RabbitMQ Hello World
